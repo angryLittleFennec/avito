c97585f9e7bba1482a5e591f374f9906ed65a360
Kristina Shcherbina <13x.skat.x13@gmail.com>
2024-04-14 21:20:38 +0300

    final fix

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Wait for a little while to let the application start and run migrations
-sleep 10
-
-# Now run the SQL script
-psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /docker-entrypoint-initdb.d/init.sql
